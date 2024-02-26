a4df2363a0b1fa956b4d460869ae6f27b0aa56b6
RaphBv <raphaelbouvier5@gmail.com>
2024-02-26 11:15:35 +0100

    Imanager

---
+++ Celeste/IManageable.h
@@ -0,0 +1,38 @@
+#pragma once
+
+template<typename ID>
+class IManageable
+{
+protected:
+	ID id;
+	bool toRemove;
+
+public:
+	void SetID(const ID& _id)
+	{
+		id = _id;
+	}
+
+	ID GetID()const
+	{
+		return id;
+	}
+	void SetToRemove(const bool _status)
+	{
+		toRemove = _status;
+	}
+	bool IsToRemove() const
+	{
+		return toRemove;
+	}
+
+public:
+	IManageable(const ID& _id)
+	{
+		id = _id;
+		toRemove = false;
+	}
+
+public:
+	virtual void Register() = 0;
+};
