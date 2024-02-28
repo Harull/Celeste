cae8119e531e12c8a8df958c8b694cf50c431a3c
MedyHanne <Hannemedy@live.fr>
2024-02-28 14:16:47 +0100

    music

---
+++ Celeste/SoundData.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "IManageable.h"
+
+#include <SFML/Audio.hpp>
+
+using namespace sf;
+using namespace std;
+
+class SoundManager;
+
+class SoundData : public SoundBuffer, public IManageable<string>
+{
+
+public:
+	SoundData(const string& _path);
+
+public:
+	virtual void Register() override;
+};
+
+
