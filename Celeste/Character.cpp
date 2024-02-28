40471036a8c4a6e365ed34aa40cf120167c8c5d9
Tranzate <enzo66200@gmail.com>
2024-02-28 17:02:32 +0100

    up background

--- Celeste/Character.cpp
+++ Celeste/Character.cpp
@@ -10,7 +10,7 @@
 
 Character::Character(const sf::Vector2f _size, const sf::Vector2f _position, const bool _isVisible)
 	: Entity(EntityData("Character", ENTITY_CHARACTER, _position, _size),
-		{ new MovementComponent(this, 2.f, sf::Vector2f(0,0), true),
+		{ new MovementComponent(this, 0.5f, sf::Vector2f(0,0), true),
 			new GravityComponent(this, 0.8f),
 			new CollisionComponent(this)})
 {
--- Celeste/Strawberry.cpp
+++ Celeste/Strawberry.cpp
@@ -5,7 +5,7 @@ Strawberry::Strawberry(const Vector2f& _position, const Vector2f& _size, const s
 {
 	components.push_back(new AnimationComponent(
 		this, _path,
-		{ AnimationData("Idle", Vector2f(0.0f,0.0f), Vector2f(12.0f, 13.0f), READ_RIGHT, ANIM_DIR_NONE, true, 3, 0.5f) },
+		{ AnimationData("Idle", Vector2f(0.0f,0.0f), Vector2f(30.0f, 39.0f), READ_RIGHT, ANIM_DIR_NONE, true, 5, 0.5f) },
 		ANIM_DIR_NONE
 	));
 }
