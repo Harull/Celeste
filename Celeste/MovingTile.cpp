09f984d42cdb0d1c0f2e222d1d9aa0090f024095
MedyHanne <Hannemedy@live.fr>
2024-03-08 09:53:32 +0100

    moving tile

--- Celeste/MovingTile.cpp
+++ Celeste/MovingTile.cpp
@@ -10,16 +10,19 @@ MovingTile::MovingTile(const EntityType _type, const Vector2f& _position, const 
 	currentDestination = Vector2f();
 	collisionReaction = [this](int _collisionSide, int _collisionSideBinary) {Move(_collisionSide, _collisionSideBinary); };
 	indexDestination = 0;
+	activated = false;
+	
 }
 
 void MovingTile::CarryCharacter()
 {
 	Character* _character = Game::GetInstance().GetPlayer()->GetCharacter();
-	if (!_character || !(_character->GetShape()->getGlobalBounds().intersects(shape->getGlobalBounds())))
+	MovementComponent* _thisMvCp = GetComponent<MovementComponent>();
+	if (!_character || !(_character->GetShape()->getGlobalBounds().intersects(shape->getGlobalBounds()))|| !_thisMvCp->GetCanMove())
 		return;
 	if (MovementComponent* _chMvCp = Game::GetInstance().GetPlayer()->GetCharacter()->GetComponent<MovementComponent>())
 	{
-		MovementComponent* _thisMvCp = GetComponent<MovementComponent>();
+		
 		_chMvCp->Move(_thisMvCp->GetDirection() * _thisMvCp->GetVelocity() / _chMvCp->GetVelocity());
 	}
 }
@@ -29,12 +32,13 @@ void MovingTile::Move(int _collisionSide, int _collisionSideBinary)
 	if (_collisionSide != COLLIDE_UP)return;
 	if (_collisionSideBinary != ENTITY_CHARACTER)return;
 	MovementComponent* _move = GetComponent<MovementComponent>();
-	if (_move->GetCanMove())return;
+	
+	if (activated)return;
 
 	indexDestination = 1;
+	activated = true;
 	_move->SetCanMove(true);
 	UpdateDirection();
-	CarryCharacter();
 }
 
 void MovingTile::Update()
@@ -46,6 +50,10 @@ void MovingTile::Update()
 	if (indexDestination == 0)
 	{
 		_move->SetCanMove(false);
+		new Timer("Reload" + id, [this]() {
+			activated = false;
+			}, seconds(2.5), true, false);
+		
 		return;
 		
 	}
@@ -64,5 +72,5 @@ void MovingTile::Update()
 		_move->SetCanMove(true);
 		}, seconds(2.5), true, false);
 	UpdateDirection();
-
+	
 }
