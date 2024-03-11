aa65ac800540c60b42ed54c96f025a0f294c8fff
MedyHanne <Hannemedy@live.fr>
2024-03-11 15:16:18 +0100

    falling fix

--- Celeste/FallingTile.h
+++ Celeste/FallingTile.h
@@ -3,12 +3,14 @@
 class FallingTile:public Tile
 {
 	bool use;
+	Vector2f startPosition;
 public:
 	FallingTile(const EntityType _type, const Vector2f& _position, const Vector2f& _size, const string& _path, Grid* _owner);
 
 public:
 	void GetHit(int _collisionSide, int _collisionSideBinary);
 	void Update()override;
+	virtual void Reset()override;
 	void CarryCharacter();
 };
 
