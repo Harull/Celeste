836d56592c6472e9075839cd6c634a6a6074cbb3
Haru <loicgiacosa@gmail.com>
2024-03-13 16:50:34 +0100

    Level 5 début

--- Celeste/FragileWallTile.h
+++ Celeste/FragileWallTile.h
@@ -7,7 +7,7 @@ public:
 	FragileWallTile(const EntityType _type, const Vector2f& _position, const Vector2f& _size, const string& _path, Grid* _owner);
 public:
 
-	void GetHit(const int _collisionSideBinary, const int _collisionTypeBinary);
+	void GetHit(const int _collisionSideBinary, const int _collisionTypeBinary, const bool _hitAllAround = true);
 };
 
 
