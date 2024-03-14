acf9848b401d25f13d6fac88ab38959ba5b63f4d
MedyHanne <Hannemedy@live.fr>
2024-03-14 15:18:18 +0100

    animation cassette

--- Celeste/TileEnd.cpp
+++ Celeste/TileEnd.cpp
@@ -12,6 +12,11 @@ TileEnd::TileEnd(const EntityType _type, const Vector2f& _position, const Vector
 	collisionReaction = [this](int _collisionSide, int _collisionSideBinary) {GetHit(_collisionSide, _collisionSideBinary); };
 	shape->setFillColor(Color::Transparent);
 	isTangible = false;
+	components.push_back(new AnimationComponent(
+		this, "Assets/Cassette.png",
+		{ AnimationData("IddleCassette", Vector2f(0.0f,0.0f), Vector2f(72.f, 52.0f), READ_RIGHT, ANIM_DIR_NONE, true, 32, 0.1f)},
+		ANIM_DIR_NONE
+	));
 }
 
 void TileEnd::GetHit(int _collisionSide, int _collisionSideBinary)
